ab98499002b8aaf565e0edce49b1e9936c74c353
dlin04 <derricklin22xc@gmail.com>
2024-10-27 16:34:45 -0400

    fix linting

--- app/api/auth/refresh/route.ts
+++ app/api/auth/refresh/route.ts
@@ -1,3 +1,3 @@
-import { NextRequest, NextResponse } from "next/server";
+// import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request: NextRequest) {}
+// export async function POST(request: NextRequest) {}
